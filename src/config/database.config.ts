052191131917e4ab763f5b1bedd3e74556fb9371
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-16 18:20:21 +0300

    feat(database): add database name to configuration

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -9,17 +9,23 @@ export class DatabaseConfiguration extends BaseConfig {
     @IsString()
     @IsNotEmpty()
     DB_URI: string;
+
+    @IsString()
+    @IsNotEmpty()
+    DB_NAME: string;
 }
 
 
 export default registerAs('database', (): MongooseModuleFactoryOptions => {
     const config = {
-        DB_URI: env.DB_URI
+        DB_URI: env.DB_URI,
+        DB_NAME: env.DB_NAME
     }
 
     ValidateConfig(DatabaseConfiguration, config);
     
     return {
-        uri: env.DB_URI
+        uri: env.DB_URI,
+        dbName: env.DB_NAME
     }
 });
