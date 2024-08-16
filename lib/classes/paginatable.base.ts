918e0ef18b8e2c081575d76327b5b15b4ef7e898
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-16 23:39:43 +0300

    feat(gateway): add `Paginatable` class for pagination

---
+++ lib/classes/paginatable.base.ts
@@ -0,0 +1,26 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { IsNumber, IsOptional } from "class-validator";
+
+export class Paginatable {
+    @ApiPropertyOptional({ description: 'Current page', example: 1, default: 1 })
+    @IsOptional()
+    @IsNumber()
+    currentPage: number = 1;
+
+    @ApiPropertyOptional({ description: 'Items limit', example: 10, default: 10, maximum: 50, minimum: 10 })
+    @IsOptional()
+    @IsNumber()
+    itemsLimit: number = 10;
+
+
+    get page(): number {
+        if (this.currentPage < 1) return 1;
+        return this.currentPage;
+    }
+
+    get items(): number {
+        if(this.itemsLimit < 10) return 10;
+        if(this.itemsLimit > 50) return 50;
+        return this.itemsLimit;
+    }
+}
