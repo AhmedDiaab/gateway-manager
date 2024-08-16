95afc9b10d98413ac75b012a7ada28bf5bd42015
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-17 00:05:32 +0300

    fix(gateway): add transform method to transform from string to number

--- lib/classes/paginatable.base.ts
+++ lib/classes/paginatable.base.ts
@@ -1,14 +1,17 @@
 import { ApiPropertyOptional } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
 import { IsNumber, IsOptional } from "class-validator";
 
 export class Paginatable {
     @ApiPropertyOptional({ description: 'Current page', example: 1, default: 1 })
     @IsOptional()
+    @Transform(({ value }) => Number(value))
     @IsNumber()
     currentPage: number = 1;
 
     @ApiPropertyOptional({ description: 'Items limit', example: 10, default: 10, maximum: 50, minimum: 10 })
     @IsOptional()
+    @Transform(({ value }) => Number(value))
     @IsNumber()
     itemsLimit: number = 10;
 
@@ -19,8 +22,8 @@ export class Paginatable {
     }
 
     get items(): number {
-        if(this.itemsLimit < 10) return 10;
-        if(this.itemsLimit > 50) return 50;
+        if (this.itemsLimit < 10) return 10;
+        if (this.itemsLimit > 50) return 50;
         return this.itemsLimit;
     }
 }
