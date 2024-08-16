be4c825f04337369a37d4653b548151aa284dc42
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-16 19:37:54 +0300

    feat(gateway): add gateway resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
+import { GatewayModule } from './modules/gateway/gateway.module';
 
 @Module({
   imports: [
-    CommonModule
+    CommonModule,
+    GatewayModule
   ],
   controllers: [AppController],
   providers: [AppService],
