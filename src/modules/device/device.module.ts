780c3a964ae943213c662b68af1af0e86466c631
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-17 15:55:35 +0300

    feat(device): register device model in device module

--- src/modules/device/device.module.ts
+++ src/modules/device/device.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DeviceService } from './device.service';
 import { DeviceController } from './device.controller';
+import { DeviceDefinition } from './entities/device.entity';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([DeviceDefinition])
+  ],
   controllers: [DeviceController],
   providers: [DeviceService],
 })
