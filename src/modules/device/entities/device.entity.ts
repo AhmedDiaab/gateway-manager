09983702a303daf080cd7f0572f6f030b5e0a959
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-17 15:55:28 +0300

    refactor(device): update model structure

--- src/modules/device/entities/device.entity.ts
+++ src/modules/device/entities/device.entity.ts
@@ -1,1 +1,24 @@
-export class Device {}
+import { BaseEntity } from "@lib/classes/entity.base";
+import { Gateway } from "@modules/gateway/entities/gateway.entity";
+import { ModelDefinition, Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type DeviceDocument = HydratedDocument<Device>;
+@Schema()
+export class Device extends BaseEntity {
+    @Prop()
+    uid: number;
+
+    @Prop()
+    vendor: string;
+
+    @Prop()
+    status: 'online' | 'offline';
+
+    @Prop({ ref: Gateway.name })
+    gateway: string;
+}
+
+export const DeviceSchema = SchemaFactory.createForClass(Device);
+
+export const DeviceDefinition: ModelDefinition = { name: Device.name, schema: DeviceSchema };
