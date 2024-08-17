8e9849d2f8c5a2df7dc6a3189e370b5e35a5fea0
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-17 15:55:54 +0300

    feat(device): update dtos for device

--- src/modules/device/dto/create-device.dto.ts
+++ src/modules/device/dto/create-device.dto.ts
@@ -1,1 +1,24 @@
-export class CreateDeviceDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsEnum, IsNumber, IsString, MaxLength, Min, MinLength } from "class-validator";
+
+enum Status {
+    Online = 'online',
+    Offline = 'offline'
+}
+export class CreateDeviceDto {
+    @ApiProperty()
+    @IsNumber()
+    @Min(1)
+    uid: number;
+
+
+    @ApiProperty()
+    @IsString()
+    @MinLength(1)
+    @MaxLength(28)
+    vendor: string;
+
+    @ApiProperty({ enum: Status })
+    @IsEnum(Status)
+    status: 'online' | 'offline';
+}
--- src/modules/device/dto/update-device.dto.ts
+++ src/modules/device/dto/update-device.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { PickType } from '@nestjs/swagger';
 import { CreateDeviceDto } from './create-device.dto';
 
-export class UpdateDeviceDto extends PartialType(CreateDeviceDto) {}
+export class UpdateDeviceDto extends PickType(CreateDeviceDto, ['status']) { }
