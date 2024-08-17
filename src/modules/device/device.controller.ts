47ba5ba489496d0e0751642c47c51c72d042b2bd
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-17 16:47:30 +0300

    feat(device): add crud for devices

--- src/modules/device/device.controller.ts
+++ src/modules/device/device.controller.ts
@@ -1,8 +1,10 @@
-import { Controller, Get, Post, Body, Param, Delete, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, UseInterceptors, Patch } from '@nestjs/common';
 import { DeviceService } from './device.service';
 import { CreateDeviceDto } from './dto/create-device.dto';
-import { ApiParam, ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiParam, ApiTags } from '@nestjs/swagger';
 import { ValidateGatewayInterceptor } from '@modules/device/interceptors/validate-gateway.interceptor';
+import { UpdateDeviceDto } from './dto/update-device.dto';
+import { DeviceResponse } from './dto/device.response';
 
 @Controller('gateways/:serial/devices')
 @ApiTags('Devices')
@@ -12,17 +14,30 @@ export class DeviceController {
   constructor(private readonly deviceService: DeviceService) { }
 
   @Post()
+  @ApiOperation({ summary: 'Add device' })
+  @ApiCreatedResponse({ type: DeviceResponse })
   create(@Param('serial') gateway: string, @Body() payload: CreateDeviceDto) {
     return this.deviceService.create(gateway, payload);
   }
 
   @Get()
-  findAll() {
-    return this.deviceService.findAll();
+  @ApiOperation({ summary: 'List all devices for gateway' })
+  @ApiOkResponse({ type: DeviceResponse, isArray: true })
+  findAll(@Param('serial') gateway: string,) {
+    return this.deviceService.findAll(gateway);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Update device status' })
+  @ApiOkResponse()
+  toggleStatus(@Param('serial') gateway: string, @Param('id') id: string, @Body() payload: UpdateDeviceDto) {
+    return this.deviceService.update(gateway, id, payload);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.deviceService.remove(+id);
+  @ApiOperation({ summary: 'Delete device' })
+  @ApiOkResponse()
+  remove(@Param('serial') gateway: string, @Param('id') id: string) {
+    return this.deviceService.remove(gateway, id);
   }
 }
--- src/modules/device/device.service.ts
+++ src/modules/device/device.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateDeviceDto } from './dto/create-device.dto';
 import { UpdateDeviceDto } from './dto/update-device.dto';
 import { InjectModel } from '@nestjs/mongoose';
@@ -13,19 +13,20 @@ export class DeviceService {
     return this.device.create({ gateway, ...payload });
   }
 
-  findAll() {
-    return `This action returns all device`;
+  findAll(gateway: string) {
+    return this.device.find({ gateway });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} device`;
+  async update(gateway: string, id: string, payload: UpdateDeviceDto) {
+    const record = await this.device.findOne({ _id: id, gateway }).exec();
+    if (!record) throw new NotFoundException();
+    record.status = payload.status;
+    await record.save();
   }
 
-  update(id: number, updateDeviceDto: UpdateDeviceDto) {
-    return `This action updates a #${id} device`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} device`;
+  async remove(gateway: string, id: string) {
+    const record = await this.device.findOne({ _id: id, gateway }).exec();
+    if (!record) throw new NotFoundException();
+    await record.deleteOne();
   }
 }
