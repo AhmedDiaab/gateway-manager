accf750499da402e0658710436f73bd8632cf6da
AhmedDiaab <ahmed.diaab.s.e@gmail.com>
2024-08-17 16:22:01 +0300

    feat(device): add logic for validating gateway attached devices

--- src/modules/device/interceptors/validate-gateway.interceptor.ts
+++ src/modules/device/interceptors/validate-gateway.interceptor.ts
@@ -1,11 +1,15 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler, NotFoundException } from '@nestjs/common';
+import { Injectable, NestInterceptor, ExecutionContext, CallHandler, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Gateway } from '../../gateway/entities/gateway.entity';
+import { Device } from '../entities/device.entity';
 
 @Injectable()
 export class ValidateGatewayInterceptor implements NestInterceptor {
-    constructor(@InjectModel(Gateway.name) private gateway: Model<Gateway>) { }
+    constructor(
+        @InjectModel(Gateway.name) private gateway: Model<Gateway>,
+        @InjectModel(Device.name) private device: Model<Device>,
+    ) { }
     async intercept(context: ExecutionContext, next: CallHandler): Promise<any> {
         // get request
         const request = context.switchToHttp().getRequest();
@@ -14,9 +18,14 @@ export class ValidateGatewayInterceptor implements NestInterceptor {
         const serial = request.params.serial;
 
         // check if gateway exists then if not throw exception
-        const count = await this.gateway.countDocuments({ serial });
+        const countPromise = this.gateway.countDocuments({ serial });
+        const devicesPromise = this.device.countDocuments({ gateway: serial });
+
+        const [count, devices] = await Promise.all([countPromise, devicesPromise]);
 
         if (count === 0) throw new NotFoundException('Gateway Not exist');
+
+        if(devices === 10) throw new BadRequestException('Gateway maximum attached devices is 10');
 
         return next.handle();
     }
